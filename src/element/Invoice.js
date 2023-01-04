7f5974c4ea842f7528a1d8788c356fde521f14cf
Kieran <kieran@harkin.me>
2023-01-04 20:09:35 +0000

    Catch invoice parsing errors

--- src/element/Invoice.js
+++ src/element/Invoice.js
@@ -1,18 +1,20 @@
 import "./Invoice.css";
 import { decode as invoiceDecode } from "light-bolt11-decoder";
 import { useMemo } from "react";
-import { useNavigate } from "react-router-dom";
 
 export default function Invoice(props) {
     const invoice = props.invoice;
-    const navigate = useNavigate();
 
     const info = useMemo(() => {
-        let parsed = invoiceDecode(invoice);
+        try {
+            let parsed = invoiceDecode(invoice);
 
-        let amount = parseInt(parsed.sections.find(a => a.name === "amount")?.value);
-        return {
-            amount: !isNaN(amount) ? (amount / 1000) : null
+            let amount = parseInt(parsed.sections.find(a => a.name === "amount")?.value);
+            return {
+                amount: !isNaN(amount) ? (amount / 1000) : null
+            }
+        } catch (e) {
+            console.error(e);
         }
     }, [invoice]);
 
