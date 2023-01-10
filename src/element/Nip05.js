a9f474fb9bcf80d82a27e4269ff7f14430e87d57
Alejandro Gomez <alejandro@strike.me>
2023-01-10 07:18:20 +0100

    address review comments

--- src/element/Nip05.js
+++ src/element/Nip05.js
@@ -1,37 +1,53 @@
 import { useState, useEffect } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
 
 import './Nip05.css'
 
 const Nip05 = ({ nip05, pubkey }) => {
     const [nip05pubkey, setNip05pubkey] = useState()
+    const [couldNotVerify, setCouldNotVerify] = useState(false)
     const isVerified = nip05pubkey === pubkey
-    const [nick, domain] = nip05.split('@')
+    const [name, domain] = nip05.split('@')
+    const isDefaultUser = name === '_'
 
     useEffect(() => {
-        fetch(`https://${domain}/.well-known/nostr.json`)
+        setCouldNotVerify(false)
+        fetch(`https://${domain}/.well-known/nostr.json?name=${name}`)
           .then((res) => res.json())
           .then(({ names }) => {
-            if (names && names[nick]) {
-              setNip05pubkey(names[nick])
+            if (names && names[name]) {
+              setNip05pubkey(names[name])
             }
           })
           .catch((err) => {
+            setCouldNotVerify(true)
             console.error("Couldn't verifiy nip05")
           })
-    }, [nip05, nick, domain])
+    }, [nip05, name, domain])
 
     return (
        <div className="flex nip05">
-         <div className="nick">{nick}</div>
-         <div className="domain">@{domain}</div>
+         {!isDefaultUser && <div className="nick">{name}</div>}
+         <div className="domain">
+             {!isDefaultUser && '@'}
+             {domain}
+         </div>
          {isVerified && (
            <span className="badge">
              <FontAwesomeIcon
                color={"green"}
                icon={faCheck}
+               size="xs"
+             />
+           </span>
+         )}
+         {couldNotVerify && (
+           <span className="error">
+             <FontAwesomeIcon
+               color={"red"}
+               icon={faTriangleExclamation}
                size="xs"
              />
            </span>
