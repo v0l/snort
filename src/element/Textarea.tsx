e209066c2dc1a38e0ea97d16c147beb5f0650265
Alejandro Gomez <alejandro@strike.me>
2023-01-18 23:16:43 +0100

    emoji autocompletion

--- src/element/Textarea.tsx
+++ src/element/Textarea.tsx
@@ -4,6 +4,7 @@ import "./Textarea.css";
 import { useState } from "react";
 import { useLiveQuery } from "dexie-react-hooks";
 import ReactTextareaAutocomplete from "@webscopeio/react-textarea-autocomplete";
+import emoji from "@jukben/emoji-search";
 import TextareaAutosize from "react-textarea-autosize";
 
 import Avatar from "./Avatar";
@@ -11,6 +12,20 @@ import Nip05 from "./Nip05";
 import { hexToBech32 } from "../Util";
 import { db } from "../db";
 import { MetadataCache } from "../db/User";
+
+interface EmojiItemProps {
+  name: string
+  char: string
+}
+
+const EmojiItem = ({ entity: { name, char } }: { entity: EmojiItemProps }) => {
+  return (
+    <div className="emoji-item">
+      <div className="emoji">{char}</div>
+      <div className="emoji-name">{name}</div>
+    </div>
+  )
+}
 
 const UserItem = (metadata: MetadataCache) => {
   const { pubkey, display_name, picture, nip05, ...rest } = metadata
@@ -46,6 +61,12 @@ const Textarea = ({ users, onChange, ...rest }: any) => {
     return allUsers
   }
 
+  const emojiDataProvider = (token: string) => {
+    return emoji(token)
+      .slice(0, 10)
+      .map(({ name, char }) => ({ name, char }));
+  }
+
   return (
     <ReactTextareaAutocomplete
       {...rest}
@@ -54,6 +75,11 @@ const Textarea = ({ users, onChange, ...rest }: any) => {
       onChange={onChange}
       textAreaComponent={TextareaAutosize}
       trigger={{
+        ":": {
+          dataProvider: emojiDataProvider,
+          component: EmojiItem,
+          output: (item: EmojiItemProps, trigger) => item.char
+        },
         "@": {
           afterWhitespace: true,
           dataProvider: userDataProvider,
