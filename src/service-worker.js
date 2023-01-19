1d56c47322cfa935fd7b33f3e6495f1ede0b82bc
Kieran <kieran@harkin.me>
2023-01-19 11:56:11 +0000

    Check host only

--- src/service-worker.js
+++ src/service-worker.js
@@ -31,7 +31,7 @@ registerRoute(({ request, url }) => url.origin === self.location.origin && stati
 
 // External media domains which have unique urls (never changing content) and can be cached forever
 const externalMediaHosts = ["void.cat", "nostr.build", "imgur.com", "i.imgur.com", "pbs.twimg.com", "i.ibb.co"];
-registerRoute(({ request, url }) => externalMediaHosts.includes(url.host) && staticTypes.includes(request.destination),
+registerRoute(({ url }) => externalMediaHosts.includes(url.host),
   new CacheFirst({
     cacheName: "ext-content-hosts"
   }));
