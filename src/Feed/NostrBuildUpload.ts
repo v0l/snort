bc16134f6d8f90348f12f0815fc19388b7554f53
Kieran <kieran@harkin.me>
2023-01-29 19:52:26 +0000

    fix upload url

--- src/Feed/NostrBuildUpload.ts
+++ src/Feed/NostrBuildUpload.ts
@@ -5,19 +5,17 @@ export default async function NostrBuildUpload(file: File | Blob): Promise<Uploa
     fd.append("fileToUpload", file);
     fd.append("submit", "Upload Image");
 
-    let rsp = await fetch("https://nostr.build/api/upload/", {
+    let rsp = await fetch("https://nostr.build/api/upload/snort.php", {
         body: fd,
         method: "POST",
         headers: {
-            "content-type": "multipart/form-data",
             "accept": "application/json"
         }
     });
     if(rsp.ok) {
         let data = await rsp.json();
-        console.debug(data);
         return {
-            url: data.url
+            url: new URL(data).toString()
         }
     }
     return {
