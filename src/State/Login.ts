eba4c19008f56ddc86bb79728450e27c48d39177
Kieran <kieran@harkin.me>
2023-01-31 23:18:31 +0000

    chore: reduce load

--- src/State/Login.ts
+++ src/State/Login.ts
@@ -139,6 +139,12 @@ export interface LoginStore {
     preferences: UserPreferences
 };
 
+const DefaultImgProxy = {
+    url: "https://imgproxy.snort.social",
+    key: "a82fcf26aa0ccb55dfc6b4bd6a1c90744d3be0f38429f21a8828b43449ce7cebe6bdc2b09a827311bef37b18ce35cb1e6b1c60387a254541afa9e5b4264ae942",
+    salt: "a897770d9abf163de055e9617891214e75a9016d748f8ef865e6ffbcb9ed932295659549773a22a019a5f06d0b440c320be411e3fddfe784e199e4f03d74bd9b"
+};
+
 export const InitState = {
     loggedOut: undefined,
     publicKey: undefined,
@@ -155,18 +161,14 @@ export const InitState = {
     dms: [],
     dmInteraction: 0,
     preferences: {
-        enableReactions: true,
+        enableReactions: false,
         autoLoadMedia: true,
         theme: "system",
         confirmReposts: false,
         showDebugMenus: false,
         autoShowLatest: false,
         fileUploader: "void.cat",
-        imgProxyConfig: {
-            url: "https://imgproxy.snort.social",
-            key: "a82fcf26aa0ccb55dfc6b4bd6a1c90744d3be0f38429f21a8828b43449ce7cebe6bdc2b09a827311bef37b18ce35cb1e6b1c60387a254541afa9e5b4264ae942",
-            salt: "a897770d9abf163de055e9617891214e75a9016d748f8ef865e6ffbcb9ed932295659549773a22a019a5f06d0b440c320be411e3fddfe784e199e4f03d74bd9b"
-        }
+        imgProxyConfig: null
     }
 } as LoginStore;
 
