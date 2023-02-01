69a4dfffb7fc8f8087096f560a403719ca46914e
Alejandro <alejandro@strike.me>
2023-02-01 23:42:46 +0100

    Ln invoice styling (#187)

--- src/Icons/ArrowBack.tsx
+++ src/Icons/ArrowBack.tsx
@@ -1,7 +1,7 @@
 const ArrowBack = () => {
   return (
     <svg width="16" height="13" viewBox="0 0 16 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M14.6667 6.5H1.33334M1.33334 6.5L6.33334 11.5M1.33334 6.5L6.33334 1.5" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M14.6667 6.5H1.33334M1.33334 6.5L6.33334 11.5M1.33334 6.5L6.33334 1.5" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   )
 }
