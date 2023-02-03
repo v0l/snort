a1340868078490a9b29cc9e0780a229118a59102
Alejandro Gomez <alejandro@strike.me>
2023-02-03 22:59:35 +0100

    fix: number format

--- src/Number.ts
+++ src/Number.ts
@@ -7,8 +7,8 @@ export function formatShort(n: number) {
   if (n < 999) {
     return n
   } else if (n < 1e8) {
-    return `${intl.format(Math.floor(n / 1e3))}K`
+    return `${intl.format(n / 1e3)}K`
   } else {
-    return `${intl.format(Math.floor(n / 1e6))}M`
+    return `${intl.format(n / 1e6)}M`
   }
 }
