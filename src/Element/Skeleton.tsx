72ab0e25b4550d65fe5fae90b3ec834b65d16f5e
Leonardo Tuna <tunaleo@gmail.com>
2023-02-06 16:05:22 -0300

    Skeleton component on timeline loading for better user experience (#190)
    * the LoadMore component should accept children to have something different than "Loading..."
    
    * skeleton component
    
    * the timeline component now loads with a skeleton
    
    * border radius for skeleton
    
    * dark mode for skeleton

---
+++ src/Element/Skeleton.tsx
@@ -0,0 +1,30 @@
+import "./Skeleton.css";
+
+interface ISkepetonProps {
+  children?: React.ReactNode;
+  loading?: boolean;
+  width?: string;
+  height?: string;
+  margin?: string;
+}
+
+export default function Skeleton({
+  children,
+  width,
+  height,
+  margin,
+  loading = true,
+}: ISkepetonProps) {
+  if (!loading) {
+    return <>{children}</>;
+  }
+
+  return (
+    <div
+      className="skeleton"
+      style={{ width: width, height: height, margin: margin }}
+    >
+      {children}
+    </div>
+  );
+}
