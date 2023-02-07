1fd934065ed1bd294099489e3f64da40f747b6cb
Alejandro Gomez <alejandro@strike.me>
2023-02-07 13:02:06 +0100

    fix: show millions correctly

--- src/Number.ts
+++ src/Number.ts
@@ -6,7 +6,7 @@ const intl = new Intl.NumberFormat("en", {
 export function formatShort(n: number) {
   if (n < 2e3) {
     return n
-  } else if (n < 1e8) {
+  } else if (n < 1e6) {
     return `${intl.format(n / 1e3)}K`
   } else {
     return `${intl.format(n / 1e6)}M`
