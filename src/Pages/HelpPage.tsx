94e692505f6dbd059e2f458848d7261ce6f838f3
Kieran <kieran@harkin.me>
2023-02-11 18:08:47 +0000

    chore: help page strings

--- src/Pages/HelpPage.tsx
+++ src/Pages/HelpPage.tsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
 import { KieranPubKey } from "Const";
+import { FormattedMessage } from "react-intl";
 
 export default function HelpPage() {
   return (
     <>
-      <h2>NIP-05</h2>
+      <h2>
+        <FormattedMessage defaultMessage="NIP-05" />
+      </h2>
       <p>
-        If you have an enquiry about your NIP-05 order please DM <Link to={`/messages/${KieranPubKey}`}>Kieran</Link>
+        <FormattedMessage
+          defaultMessage="If you have an enquiry about your NIP-05 order please DM {link}"
+          values={{
+            link: <Link to={`/messages/${KieranPubKey}`}>Kieran</Link>,
+          }}
+        />
       </p>
     </>
   );
