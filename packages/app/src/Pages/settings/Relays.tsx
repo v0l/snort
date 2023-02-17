3f2bea80035578d7d4775f7ef77e837ed4199e6d
Fernando López Guevara <fernando.lguevara@gmail.com>
2023-02-17 17:53:43 -0300

    feat(relays): add export button and handle multiple relay urls

--- packages/app/src/Pages/settings/Relays.tsx
+++ packages/app/src/Pages/settings/Relays.tsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import { FormattedMessage } from "react-intl";
 import { useDispatch, useSelector } from "react-redux";
 
-import { randomSample } from "Util";
+import { RelaySettings } from "@snort/nostr";
 import Relay from "Element/Relay";
 import useEventPublisher from "Feed/EventPublisher";
-import { RootState } from "State/Store";
-import { RelaySettings } from "@snort/nostr";
 import { setRelays } from "State/Login";
+import { RootState } from "State/Store";
+import { randomSample } from "Util";
 
 import messages from "./messages";
 
@@ -31,6 +31,10 @@ const RelaySettingsPage = () => {
     }
   }
 
+  function exportRelays() {
+    navigator.clipboard.writeText(Object.keys(relays).join("\r\n"));
+  }
+
   function addRelay() {
     return (
       <>
@@ -41,29 +45,72 @@ const RelaySettingsPage = () => {
           <input
             type="text"
             className="f-grow"
-            placeholder="wss://my-relay.com"
+            placeholder="wss://relay.com wss://relay1.com;wss://relay2.com,wss://relay3.com"
             value={newRelay}
             onChange={e => setNewRelay(e.target.value)}
           />
         </div>
-        <button className="secondary mb10" onClick={() => addNewRelay()}>
-          <FormattedMessage {...messages.Add} />
-        </button>
+        <small className="mb10">
+          <FormattedMessage {...messages.AddRelaysHelp} />
+        </small>
+        <div className="flex">
+          <div className="f-grow"></div>
+          <button className="secondary mb10" onClick={() => addNewRelay()}>
+            <FormattedMessage {...messages.AddRelays} />
+          </button>
+        </div>
       </>
     );
   }
 
   function addNewRelay() {
-    if ((newRelay?.length ?? 0) > 0) {
-      const parsed = new URL(newRelay ?? "");
+    if (newRelay?.length) {
+      let seps = "; ,".split("");
+      let newRelays: string[] = [newRelay];
+
+      if ((seps = seps.filter(s => newRelay.includes(s))).length) {
+        // newRalay into arary
+        newRelays = seps.flatMap(sep => newRelay.split(sep));
+      }
+
+      const parsedRelays = newRelays
+        .map((url: string) => {
+          url = url?.trim() ?? undefined;
+          try {
+            if (url && !url.startsWith("ws")) {
+              url = `wss://${url}`;
+            }
+            return new URL(url).toString();
+          } catch (_) {
+            // noop
+          }
+          return "";
+        })
+        .filter(Boolean);
+
+      console.log("parsed relays:", parsedRelays);
+
       const payload = {
         relays: {
-          ...relays,
-          [parsed.toString()]: { read: false, write: false },
+          ...parsedRelays.reduce(
+            (c: Record<string, { read: boolean; write: boolean }>, key: string) => {
+              key = key?.endsWith("/") ? key.substring(0, key.length - 1) : key;
+              return (
+                (!Object.keys(c).some(k => !k.includes(key)) &&
+                  (c[key] = c[key] || { read: false, write: false }) &&
+                  false) ||
+                c
+              );
+            },
+            { ...relays }
+          ),
         },
         createdAt: Math.floor(new Date().getTime() / 1000),
       };
+
       dispatch(setRelays(payload));
+
+      setNewRelay("");
     }
   }
 
@@ -79,6 +126,9 @@ const RelaySettingsPage = () => {
       </div>
       <div className="flex mt10">
         <div className="f-grow"></div>
+        <button className="mr10" type="button" onClick={() => exportRelays()}>
+          <FormattedMessage {...messages.ExportToClipboard} />
+        </button>
         <button type="button" onClick={() => saveRelays()}>
           <FormattedMessage {...messages.Save} />
         </button>
