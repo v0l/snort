45999371768e5e356433fe8063e3bfa82d574b03
Kieran <kieran@harkin.me>
2023-02-28 19:48:06 +0000

    bug: strokeWidth on spinner

--- packages/app/src/Icons/Spinner.tsx
+++ packages/app/src/Icons/Spinner.tsx
@@ -4,7 +4,7 @@ import "./Spinner.css";
 const Spinner = (props: IconProps) => (
   <svg width="20" height="20" stroke="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...props}>
     <g className="spinner_V8m1">
-      <circle cx="10" cy="10" r="7.5" fill="none" stroke-width="3"></circle>
+      <circle cx="10" cy="10" r="7.5" fill="none" strokeWidth="3"></circle>
     </g>
   </svg>
 );
