27edf5f592eae4d61cbbb7d61caf0a2cbb3b3b72
Kieran <kieran@harkin.me>
2023-03-03 11:56:03 +0000

    bug: zap button not working

--- packages/app/src/Element/ZapButton.tsx
+++ packages/app/src/Element/ZapButton.tsx
@@ -2,25 +2,20 @@ import "./ZapButton.css";
 import { faBolt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
-import { useLongPress } from "use-long-press";
+import { HexKey } from "@snort/nostr";
 
 import { useUserProfile } from "Feed/ProfileFeed";
-import { HexKey } from "@snort/nostr";
 import SendSats from "Element/SendSats";
 
 const ZapButton = ({ pubkey, lnurl }: { pubkey: HexKey; lnurl?: string }) => {
   const profile = useUserProfile(pubkey);
   const [zap, setZap] = useState(false);
   const service = lnurl ?? (profile?.lud16 || profile?.lud06);
-  const longPress = useLongPress(() => {
-    console.debug("long press");
-  });
-
   if (!service) return null;
 
   return (
     <>
-      <div className="zap-button" {...longPress()}>
+      <div className="zap-button" onClick={() => setZap(true)}>
         <FontAwesomeIcon icon={faBolt} />
       </div>
       <SendSats
