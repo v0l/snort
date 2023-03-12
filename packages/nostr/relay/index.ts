f597c3a456dae29f1ed36a7e3098517cc860adc3
ennmichael <enntheprogrammer@gmail.com>
2023-03-13 00:19:24 +0100

    cleanup

--- packages/nostr/relay/index.ts
+++ packages/nostr/relay/index.ts
@@ -1,3 +1,13 @@
+/**
+ * Allows the relay to be shut down with an HTTP request, after which
+ * docker-compose will restart it. This allows each test to have a clean
+ * slate. The drawback is that the tests can't run in parallel, so the
+ * test suite is very slow. A better option would be to have this relay
+ * server manage the relay completely: star/stop isolated relay instances
+ * with HTTP requests and allow multiple instances to run at the same
+ * time so that the tests can be parallelized.
+ */
+
 import http from "node:http"
 import { spawn } from "node:child_process"
 
