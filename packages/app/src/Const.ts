a21e99fff8a4c200a2bf0efb7d5410bd3c0ab4e1
Michael Rhee <michaelrhee@pm.me>
2023-03-29 16:01:54 -0500

    Update Wavlake embed to support .com links

--- packages/app/src/Const.ts
+++ packages/app/src/Const.ts
@@ -168,5 +168,4 @@ export const MagnetRegex = /(magnet:[\S]+)/i;
 /**
  * Wavlake embed regex
  */
-export const WavlakeRegex =
-  /player\.wavlake\.com\/(?!feed\/)(track\/[.a-zA-Z0-9-]+|album\/[.a-zA-Z0-9-]+|[.a-zA-Z0-9-]+)/i;
+export const WavlakeRegex = /(?:player\.)?wavlake\.com\/(track\/[.a-zA-Z0-9-]+|album\/[.a-zA-Z0-9-]+|[.a-zA-Z0-9-]+)/i;
