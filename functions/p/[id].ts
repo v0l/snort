f5e6ef679b62d352be5d39247be8ef93a35d9ab2
Kieran <kieran@harkin.me>
2023-03-30 12:54:01 +0100

    chore: cleanup functions

--- functions/p/[id].ts
+++ functions/p/[id].ts
@@ -5,21 +5,26 @@ export const onRequest: PagesFunction<Env> = async (context) => {
   const id = context.params.id as string;
 
   const next = await context.next();
-  console.debug(next.status);
-  console.debug("Fetching data for: ", id);
-
-  const rsp = await fetch(`https://api.snort.social/api/v1/og/tag/p/${id}`, {
-    method: "POST",
-    body: await next.arrayBuffer(),
-    headers: {
-      "content-type": "text/plain"
+  try {
+    const rsp = await fetch(`https://api.snort.social/api/v1/og/tag/p/${id}`, {
+      method: "POST",
+      body: await next.arrayBuffer(),
+      headers: {
+        "content-type": "text/plain"
+      }
+    });
+    if (rsp.ok) {
+      const body = await rsp.text();
+      if (body.length > 0) {
+        return new Response(body, {
+          headers: {
+            "content-type": "text/html"
+          }
+        });
+      }
     }
-  });
-  console.debug(rsp.status, rsp.statusText);
-  const body = await rsp.text();
-  return new Response(body, {
-    headers: {
-      "content-type": "text/html"
-    }
-  });
+  } catch {
+    // ignore
+  }
+  return next;
 }
