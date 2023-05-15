4fdea1fdbdde9181e6350c2c3747d121091d6486
Michael Rhee <michaelrhee@pm.me>
2023-05-15 14:08:08 -0500

    Add profiles to non-matching wavlake urls

--- packages/app/src/Const.ts
+++ packages/app/src/Const.ts
@@ -178,7 +178,7 @@ export const MagnetRegex = /(magnet:[\S]+)/i;
  * Wavlake embed regex
  */
 export const WavlakeRegex =
-  /https?:\/\/(?:player\.|www\.)?wavlake\.com\/(?!top|new|artists|account|activity|login|preferences|feed)(?:(?:track|album)\/[a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12}|[a-z-]+)/i;
+  /https?:\/\/(?:player\.|www\.)?wavlake\.com\/(?!top|new|artists|account|activity|login|preferences|feed|profile)(?:(?:track|album)\/[a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12}|[a-z-]+)/i;
 
 /*
  * Regex to match any base64 string
