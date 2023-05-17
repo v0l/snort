6e16e7b5fbdb9bd38b2c516f57bb6a41d8ffa38e
Kieran <kieran@harkin.me>
2023-05-17 11:37:48 +0100

    feat: improve error page

--- packages/app/src/Pages/ErrorPage.tsx
+++ packages/app/src/Pages/ErrorPage.tsx
@@ -1,3 +1,5 @@
+import { db } from "Db";
+import AsyncButton from "Element/AsyncButton";
 import { FormattedMessage } from "react-intl";
 import { useRouteError } from "react-router-dom";
 
@@ -6,12 +8,28 @@ const ErrorPage = () => {
 
   console.error(error);
   return (
-    <>
+    <div className="main-content page">
       <h4>
         <FormattedMessage defaultMessage="An error has occured!" />
       </h4>
-      <pre>{JSON.stringify(error, undefined, "  ")}</pre>
-    </>
+      <AsyncButton
+        onClick={async () => {
+          await db.delete();
+          globalThis.localStorage.clear();
+          globalThis.location.href = "/";
+        }}>
+        <FormattedMessage defaultMessage="Clear cache and reload" />
+      </AsyncButton>
+      {
+        <pre>
+          {JSON.stringify(
+            error instanceof Error ? { name: error.name, message: error.message, stack: error.stack } : error,
+            undefined,
+            "  "
+          )}
+        </pre>
+      }
+    </div>
   );
 };
 
