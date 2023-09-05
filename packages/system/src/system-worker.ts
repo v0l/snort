f9d2a22c7431371ac513b5fba388a5f50a9a3d5d
Kieran <kieran@harkin.me>
2023-09-05 15:07:06 +0100

    Fix build

--- packages/system/src/system-worker.ts
+++ packages/system/src/system-worker.ts
@@ -2,8 +2,8 @@ import { ExternalStore } from "@snort/shared";
 
 import { SystemSnapshot, SystemInterface, ProfileLoaderService } from ".";
 import { AuthHandler, ConnectionStateSnapshot, RelaySettings } from "./connection";
-import { NostrEvent } from "./nostr";
-import { NoteStore } from "./note-collection";
+import { NostrEvent, TaggedNostrEvent } from "./nostr";
+import { NoteStore, NoteStoreSnapshotData } from "./note-collection";
 import { Query } from "./query";
 import { RequestBuilder } from "./request-builder";
 
@@ -21,7 +21,7 @@ export class SystemWorker extends ExternalStore<SystemSnapshot> implements Syste
     }
   }
   
-  Fetch(req: RequestBuilder): Promise<Query> {
+  Fetch(req: RequestBuilder, cb?: (evs: Array<TaggedNostrEvent>) => void) : Promise<NoteStoreSnapshotData> {
     throw new Error("Method not implemented.");
   }
 
