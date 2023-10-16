d9fc4f37b0e5f1e4a8cc2ad5ec4b34ee04db8f9a
Kieran <kieran@harkin.me>
2023-10-16 10:09:08 +0000

    chore: Update translations

--- packages/app/src/Element/Progress.tsx
+++ packages/app/src/Element/Progress.tsx
@@ -3,16 +3,18 @@ import "./Progress.css";
 import { CSSProperties } from "react";
 
 export default function Progress({ value }: { value: number }) {
-    const v = Math.max(0.01, Math.min(1, value));
-    return <div className="progress">
-        <div
-            style={
-                {
-                    "--progress": `${v * 100}%`,
-                } as CSSProperties
-            }></div>
-        <span>
-            <FormattedNumber value={v} style="percent" />
-        </span>
+  const v = Math.max(0.01, Math.min(1, value));
+  return (
+    <div className="progress">
+      <div
+        style={
+          {
+            "--progress": `${v * 100}%`,
+          } as CSSProperties
+        }></div>
+      <span>
+        <FormattedNumber value={v} style="percent" />
+      </span>
     </div>
+  );
 }
