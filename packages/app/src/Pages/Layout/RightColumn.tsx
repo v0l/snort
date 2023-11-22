e97b9bdba4ce33fc11bb35feba8b3d9e6df974df
Martti Malmi <sirius@iki.fi>
2023-11-22 14:13:40 +0200

    scrollable trending column

--- packages/app/src/Pages/Layout/RightColumn.tsx
+++ packages/app/src/Pages/Layout/RightColumn.tsx
@@ -6,27 +6,29 @@ import { FormattedMessage } from "react-intl";
 
 export default function RightColumn() {
   return (
-    <div className="flex-col hidden lg:flex lg:w-1/3 sticky top-0 h-screen p-2 border-l border-neutral-900 gap-8">
-      <div>
+    <div className="flex-col hidden lg:flex lg:w-1/3 h-screen p-2 border-l border-neutral-900 gap-8">
+      <div className="sticky top-0">
         <SearchBox />
       </div>
-      <div>
-        <div className="font-bold">
-          <FormattedMessage defaultMessage="Trending hashtags" id="CbM2hK" />
+      <div className="overflow-y-auto flex-grow">
+        <div>
+          <div className="font-bold">
+            <FormattedMessage defaultMessage="Trending hashtags" id="CbM2hK" />
+          </div>
+          <TrendingHashtags count={5} />
         </div>
-        <TrendingHashtags count={5} />
-      </div>
-      <div>
-        <div className="font-bold">
-          <FormattedMessage defaultMessage="Trending notes" id="6k7xfM" />
+        <div>
+          <div className="font-bold">
+            <FormattedMessage defaultMessage="Trending notes" id="6k7xfM" />
+          </div>
+          <TrendingNotes count={5} />
         </div>
-        <TrendingNotes count={5} />
-      </div>
-      <div>
-        <div className="font-bold">
-          <FormattedMessage defaultMessage="Trending users" id="arZnG2" />
+        <div>
+          <div className="font-bold">
+            <FormattedMessage defaultMessage="Trending users" id="arZnG2" />
+          </div>
+          <TrendingUsers count={5} />
         </div>
-        <TrendingUsers count={5} />
       </div>
     </div>
   );
