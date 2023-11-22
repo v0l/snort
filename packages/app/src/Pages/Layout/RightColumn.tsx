77984f9124fdfc719221eadf240a6b24aa2334b7
Martti Malmi <sirius@iki.fi>
2023-11-22 10:27:54 +0200

    right col border

--- packages/app/src/Pages/Layout/RightColumn.tsx
+++ packages/app/src/Pages/Layout/RightColumn.tsx
@@ -2,7 +2,7 @@ import SearchBox from "../../Element/SearchBox";
 
 export default function RightColumn() {
   return (
-    <div className="flex-col hidden lg:flex lg:w-1/3 sticky top-0 h-screen p-2">
+    <div className="flex-col hidden lg:flex lg:w-1/3 sticky top-0 h-screen p-2 border-l border-neutral-900">
       <div>
         <SearchBox />
       </div>
