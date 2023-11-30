a4f691d2607108442e967ddb2c946fc2d5ec5a54
Martti Malmi <sirius@iki.fi>
2023-11-30 13:03:22 +0200

    disable media and line breaks in trending notes

--- packages/app/src/Element/Trending/ShortNote.tsx
+++ packages/app/src/Element/Trending/ShortNote.tsx
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 import NoteTime from "@/Element/Event/NoteTime";
 
 export default function ShortNote({ event }: { event: TaggedNostrEvent }) {
+  // replace newlines with spaces, replace double spaces with single spaces
+  const content = event.content.slice(0, 80).replace(/\n/g, " ").replace(/  +/g, " ");
   return (
     <Link to={`/${NostrLink.fromEvent(event).encode(CONFIG.eventLinkPrefix)}`} className="flex flex-col">
       <div className="flex flex-row justify-between">
@@ -12,7 +14,7 @@ export default function ShortNote({ event }: { event: TaggedNostrEvent }) {
         <NoteTime from={event.created_at * 1000} />
       </div>
       <div className="ml-10">
-        <Text {...event} truncate={75} />
+        <Text {...event} content={content} truncate={75} disableMedia={true} />
       </div>
     </Link>
   );
