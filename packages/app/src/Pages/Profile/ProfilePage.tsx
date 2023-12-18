a34bf6591c5122691445aacda32719d8fb92dd48
Martti Malmi <sirius@iki.fi>
2023-12-18 17:01:16 +0200

    link to settings in sm and edit in md

--- packages/app/src/Pages/Profile/ProfilePage.tsx
+++ packages/app/src/Pages/Profile/ProfilePage.tsx
@@ -321,7 +321,12 @@ export default function ProfilePage({ id: propId, state }: ProfilePageProps) {
         )}
         {isMe ? (
           <>
-            <Link to="/settings/profile">
+            <Link className="md:hidden" to="/settings">
+              <button>
+                <FormattedMessage defaultMessage="Settings" id="D3idYv" />
+              </button>
+            </Link>
+            <Link className="hidden md:inline" to="/settings/profile">
               <button>
                 <FormattedMessage defaultMessage="Edit" id="wEQDC6" />
               </button>
