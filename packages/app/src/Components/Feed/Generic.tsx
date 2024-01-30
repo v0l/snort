8c19f4de6806f176212050858bf5e588a217e5ac
Kieran <kieran@harkin.me>
2024-01-30 20:06:24 +0000

    chore: Remove here map

--- packages/app/src/Components/Feed/Generic.tsx
+++ packages/app/src/Components/Feed/Generic.tsx
@@ -1,13 +1,9 @@
-import { dedupe, removeUndefined } from "@snort/shared";
 import { NostrLink, ReqFilter, RequestBuilder } from "@snort/system";
 import { useRequestBuilder } from "@snort/system-react";
-import Geohash from "latlon-geohash";
 import { lazy, Suspense, useMemo } from "react";
 
 import { TimelineRenderer } from "@/Components/Feed/TimelineRenderer";
-import { findTag } from "@/Utils";
 const LazySimpleChart = lazy(async () => await import("@/Components/LineChart"));
-const LazyMap = lazy(async () => await import("@/Components/HereMap"));
 
 export function GenericFeed({ link }: { link: NostrLink }) {
   const reqs = JSON.parse(link.id) as Array<ReqFilter>;
@@ -23,26 +19,10 @@ export function GenericFeed({ link }: { link: NostrLink }) {
 
   const evs = useRequestBuilder(sub);
 
-  const geoTags = dedupe(removeUndefined(evs.map(a => findTag(a, "g"))));
   const isTempSensor = reqs[0].kinds?.includes(8001) && reqs[0].kinds.length === 1;
   if (isTempSensor) {
     return (
       <div className="p flex flex-col gap-2">
-        {geoTags.length > 0 && (
-          <Suspense>
-            <LazyMap
-              zoom={2}
-              center={{ lat: 30, lng: 0 }}
-              markers={geoTags.map(a => {
-                const pos = Geohash.decode(a);
-                return {
-                  lat: pos.lat,
-                  lng: pos.lon,
-                };
-              })}
-            />
-          </Suspense>
-        )}
         <Suspense>
           <LazySimpleChart
             data={evs
