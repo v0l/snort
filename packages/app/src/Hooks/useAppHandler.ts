99f76df9ee9aa769aee981cfbbb1a88ace7e6d75
Kieran <kieran@harkin.me>
2025-11-10 09:56:26 +0000

    fix: cache app handler obj

--- packages/app/src/Hooks/useAppHandler.ts
+++ packages/app/src/Hooks/useAppHandler.ts
@@ -42,7 +42,7 @@ export default function useAppHandler(kind: EventKind): Array<AppHandler> {
 
   const wot = useWoT();
   const dataRecommends = useRequestBuilder(reccomendsSub);
-  const apps = dataApps.map(a => {
+  const apps = useMemo(() => dataApps.map(a => {
     const meta = a.content.startsWith("{") && a.content.endsWith("}") ? mapEventToProfile(a) : undefined;
     const link = NostrLink.fromEvent(a);
     return {
@@ -50,7 +50,7 @@ export default function useAppHandler(kind: EventKind): Array<AppHandler> {
       metadata: meta,
       reccomendations: wot.sortEvents(dataRecommends.filter(a => link.isReplyToThis(a))),
     } as AppHandler;
-  });
+  }), [dataApps.length, dataRecommends.length, wot]);
 
   return apps.sort((a, b) => (a.reccomendations.length > b.reccomendations.length ? -1 : 1));
 }
